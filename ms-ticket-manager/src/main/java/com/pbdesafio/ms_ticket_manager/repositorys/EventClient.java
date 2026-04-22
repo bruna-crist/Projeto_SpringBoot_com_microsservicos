5b8095fb34e2ea3595dace871e349e8b8b0997df
Bruna Cristina <brunacristinabrandao43@gmail.com>
2026-04-22 15:55:54 -0300

    fix: correct Feign client URL for Docker network communication between microservices

--- ms-ticket-manager/src/main/java/com/pbdesafio/ms_ticket_manager/repositorys/EventClient.java
+++ ms-ticket-manager/src/main/java/com/pbdesafio/ms_ticket_manager/repositorys/EventClient.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-event-manager", url = "http://localhost:8080")
+@FeignClient(name = "ms-event-manager", url = "http://ms-event-manager:8080")
 public interface EventClient {
 
     @GetMapping("events/{id}")
